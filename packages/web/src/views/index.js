84be2fef03afe194960ab0c8095e13d6f9204d79
Miguel Palhas <mpalhas@gmail.com>
2021-12-27 21:06:59 +0000

    appfairy: Migrate

--- packages/web/src/views/index.js
+++ packages/web/src/views/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 
-export { default as WalletConnectView } from './WalletConnectView'
 export { default as GridView } from './GridView'
 export { default as NotFoundView } from './NotFoundView'
 export { default as IndexView } from './IndexView'
