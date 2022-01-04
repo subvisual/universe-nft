93244a21ffb1c2ad4096ca51bbd21b692be602a3
Miguel Palhas <mpalhas@gmail.com>
2022-01-04 09:50:01 +0000

    appfairy: Migrate

--- packages/web/src/views/index.js
+++ packages/web/src/views/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 
+export { default as AfActionsView } from './AfActionsView'
 export { default as AfGridView } from './AfGridView'
 export { default as NotFoundView } from './NotFoundView'
 export { default as IndexView } from './IndexView'
