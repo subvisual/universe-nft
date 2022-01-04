a6c1b74d16cbf9d7cf5be6accfbe5370195918c3
Miguel Palhas <mpalhas@gmail.com>
2022-01-04 10:01:32 +0000

    Fixing homepage

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 
-import IndexController from "./controllers/IndexController";
+import IndexView from "./views/IndexView";
 
 import { NFTProvider } from "./lib/NFTContext";
 import { MintProvider } from "./lib/MintContext";
@@ -11,7 +11,7 @@ const App: FC = () => {
   return (
     <NFTProvider>
       <MintProvider>
-        <IndexController />
+        <IndexView />
       </MintProvider>
     </NFTProvider>
   );
