1a70010be593ff945e5e117bc95f53d452db85c3
Miguel Palhas <mpalhas@gmail.com>
2022-01-06 19:43:36 +0000

    Adds missing cell

---
+++ packages/web/src/controllers/IndexController.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+import IndexView from "../views/IndexView";
+
+const IndexController: FC = () => {
+  return <IndexView />;
+};
+
+export default IndexController;
