593e2600384faeea1926a92614e5544efdf20d7f
Miguel Palhas <mpalhas@gmail.com>
2022-01-07 18:44:06 +0000

    Styling the grid

--- packages/web/src/components/Cell.tsx
+++ packages/web/src/components/Cell.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
 
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
@@ -16,11 +18,17 @@ const emptyURI = (x: number, y: number) => `/empty/${x}x${y}.png`;
 const coordsToId = (x: number, y: number) => (x << 16) + y;
 
 export const Cell: FC<Props> = ({ x, y, size }) => {
+  const { library, account } = useWeb3React<Web3Provider>();
   const id = coordsToId(x, y);
   const [uri, setUri] = useState(emptyURI(x, y));
+  const [label, setLabel] = useState("");
+  const [owner, setOwner] = useState<string | null>(null);
+  const [ens, setEns] = useState<string | null>(null);
+  const [own, setOwn] = useState<boolean>(false);
 
   const { contract } = useNFT();
 
+  // update owner and URI
   useEffect(() => {
     (async function () {
       if (!contract) return;
@@ -28,10 +36,54 @@ export const Cell: FC<Props> = ({ x, y, size }) => {
       const data = await contract.tokenData(id);
 
       if (data.owner != ethers.constants.AddressZero) {
+        setOwner(data.owner);
         setUri(data.uri);
       }
     })();
   }, [contract]);
 
-  return <img style={{ width: size, height: size }} src={uri} />;
+  // update label
+  useEffect(() => {
+    (async function () {
+      if (!owner) {
+        return;
+      }
+
+      let label = owner;
+
+      try {
+        const ens = await library!.lookupAddress(owner);
+
+        if (ens) {
+          label = ens;
+        }
+      } catch (err) {
+        console.log(err);
+      }
+
+      setLabel(label);
+    });
+  }, [owner]);
+
+  // update own
+  useEffect(() => {
+    if (!owner || !account) {
+      return;
+    }
+
+    setOwn(owner == account);
+  }, [owner, account]);
+
+  return (
+    <img
+      style={{
+        width: size,
+        height: size,
+        boxShadow: own ? "0px 2px 10px 0px rgba(0,0,0,0.5)" : "none",
+      }}
+      src={uri}
+      alt={label}
+      title={label}
+    />
+  );
 };
